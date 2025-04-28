7b9199de72c79c069f2448a2312f99541585c4f3
bhaxkar0 <hlobhaskarjha02@gmail.com>
2025-04-28 20:53:20 +0530

    feat: Integrate Tailwind CSS and update project assets
    - Replaced favicon with a new payment gateway image.
    - Added Tailwind CSS and related dependencies for styling.
    - Updated Vite configuration to include Tailwind CSS plugin.
    - Refactored App component to implement routing with react-router-dom.
    - Created new pages and components for user interactions.
    - Added global styles with Tailwind CSS and custom font imports.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,47 @@
-function App() {
-  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserProvider } from "./context/UserContextProvider";
+import { Toaster } from 'react-hot-toast';
+import PrivateRoute from "./components/common/PrivateRoute";
+import Navbar from "./components/layout/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import Transactions from "./pages/Transactions";
+import TransactionDetails from "./pages/TransactionDetails";
+import Error from "./pages/Error";
 
+
+const AppRoutes = () => {
   return (
-    <div>Hello, World!</div>
-  )
-}
+    <>
+      <Navbar />
+      <main className=" bg-indigo-50">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/send" element={<PrivateRoute><SendMoney /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route path="/transactions" element={<PrivateRoute><Transactions /></PrivateRoute>} />
+          <Route path="/transaction/:transactionId" element={<PrivateRoute><TransactionDetails /></PrivateRoute>} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </main>
+      <Toaster />
+    </>
+  );
+};
 
-export default App
+function App() {
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </UserProvider>
+  );
+};
+
+export default App;
